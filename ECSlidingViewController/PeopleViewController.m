0395da3efa12de9e8039e8c8f3ee8d054f0274df
Meng To <meng@Mengs-iMac-2.local>
2013-04-25 18:28:46 -0700

    Game

--- ECSlidingViewController/PeopleViewController.m
+++ ECSlidingViewController/PeopleViewController.m
@@ -34,8 +34,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)backButton:(id)sender {
-    [self.navigationController popViewControllerAnimated:YES];
-}
 
 @end
